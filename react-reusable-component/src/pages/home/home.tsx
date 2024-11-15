891ddd418951bef18f288aa5da48053e3f9b19bd
tanvir1017 <developer.tanvirhossain@gmail.com>
2024-11-15 10:50:35 +0600

    feat: 🎸 working on reusable Form [React Hook Form] 💡⚡

--- react-reusable-component/src/pages/home/home.tsx
+++ react-reusable-component/src/pages/home/home.tsx
@@ -1,28 +1,9 @@
-import { useRef, useState } from "react";
-import Button from "../../components/ui/Button";
-import Modal from "../../components/ui/Modal";
+import NormalForm from "../../components/normal-form/NormalForm";
 
 const Home = () => {
-  const buttonRef = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const onCloseModal = () => {
-    setIsOpen((prev) => !prev);
-  };
   return (
     <div className="relative">
-      <h1>Home page</h1>
-      <Button
-        ref={buttonRef}
-        onClick={() => setIsOpen((prev) => !prev)}
-        variant="outline"
-        className=""
-      >
-        Open Modal
-      </Button>
-      <Modal isOpen={isOpen} onCloseModal={onCloseModal}>
-        <h1>This is a modal</h1>
-      </Modal>
+      <NormalForm />
     </div>
   );
 };
--- react-reusable-component/tailwind.config.js
+++ react-reusable-component/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    theme: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+      extend: {},
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
