2fc2f26ded6b03e132597e792fc782eb95118078
Tanvir Hossain <developer.tanvirhossain@gmail.com>
2024-11-20 12:56:55 +0600

    feat: 🎸 Unfortunatly pushed motion into reusable components ❌❌

--- react-reusable-component/src/pages/admin/add-admin.tsx
+++
@@ -1,9 +0,0 @@
-const AddAdmin = () => {
-  return (
-    <div>
-      <h2>This is add admin </h2>
-    </div>
-  );
-};
-
-export default AddAdmin;
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
-import React from "react";
+import { motion } from "motion/react";
 import "./App.css";
-import { ThemeContext, TThemeValue } from "./context/ThemeProvider";
-import UserDataFetch from "./pages/userDataFetch";
+
+const parent = {
+  hidden: {
+    opacity: 0,
+    scale: 0.9,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+  hover: {
+    scale: 1.1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  tap: {
+    scale: 0.9,
+    borderRadius: "999px",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+    cursor: "pointer",
+    transition: {
+      duration: 0.8,
+    },
+  },
+};
 
 function App() {
-  const [hide, setHide] = React.useState<boolean>(false);
-  const { dark, setDark } = React.useContext(ThemeContext) as TThemeValue;
-
   return (
-    <div className={`${dark ? "bg-black " : "bg-white "} border p-10`}>
-      <UserDataFetch />
+    <div className="grid grid-flow-col place-items-center h-screen">
+      <motion.div
+        className="size-64 bg-green-600 rounded-lg flex flex-wrap gap-5 justify-center items-center p-9 "
+        variants={parent}
+        initial="hidden"
+        animate="visible"
+        whileHover="hover"
+        whileTap="tap"
+      ></motion.div>
     </div>
   );
 }
